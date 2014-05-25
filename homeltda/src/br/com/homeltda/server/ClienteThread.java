920d675f48729fd7c0ef7e9d7af1225e3b6bc79d
Lucas Almeida <lucas.almeida.las@gmail.com>
2014-05-25 16:45:42 -0300

    ServidorThread recebe novas conexões e direciona esta para uma ClienteThread;
    Classes: ServerTest -> verifica se o servidor está recebendo conexões; 
    ClientTest -> enviar informações para o servidor;

--- homeltda/src/br/com/homeltda/server/ClienteThread.java
+++ homeltda/src/br/com/homeltda/server/ClienteThread.java
@@ -1,6 +1,13 @@
 package br.com.homeltda.server;
 
+import java.net.Socket;
+
 public class ClienteThread implements Runnable {
+
+	private Socket socket; 
+	public ClienteThread(Socket socket) {
+		this.socket = socket;
+	}
 
 	@Override
 	public void run() {
--- homeltda/src/br/com/homeltda/server/ServidorThread.java
+++ homeltda/src/br/com/homeltda/server/ServidorThread.java
@@ -1,14 +1,34 @@
 package br.com.homeltda.server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServidorThread implements Runnable {
 
-	private ServerSocket server; 
+	private ServerSocket servidor; 
+	
+	public ServidorThread() throws IOException{
+		servidor = new ServerSocket(6525);
+	}
 	
 	@Override
 	public void run() {
 
+		while(true){
+			try {
+				Socket socket = servidor.accept();
+				criarClienteThread(socket);
+			} catch (IOException e) {
+				System.out.println("Erro");
+			}
+		}
+	}
+	
+	public void criarClienteThread(Socket socket){
+		ClienteThread cliente = new ClienteThread(socket); 
+		Thread t = new Thread(cliente); 
+		t.start();
 	}
 
 }
